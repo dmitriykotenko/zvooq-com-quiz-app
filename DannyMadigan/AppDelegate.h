728ba5f5ae1996f2791af30f0d6ea01215c5282b
Dmitry Kotenko <dmitry.kotenko@cyberhull.com>
2016-10-10 15:24:09 +0300

    Initial Commit

---
+++ DannyMadigan/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DannyMadigan
+//
+//  Created by Dmitry Kotenko on 10/10/2016.
+//  Copyright © 2016 Fiasko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DannyMadigan/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DannyMadigan
+//
+//  Created by Dmitry Kotenko on 10/10/2016.
+//  Copyright © 2016 Fiasko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
